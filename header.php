bc0b02d07f684b2bba0185d8096498556aae1435
Andres Vega <gydoar@hotmail.com>
2017-03-01 19:24:25 -0500

    agrenando mapa de calor

--- header.php
+++ header.php
@@ -46,6 +46,13 @@
     <meta property="og:locale:alternate" content="es_ES" />
     <meta property="og:url" content="<?php echo $url_actual; ?>" />
     <meta property="og:image" content="img/post-face.png" />
+
+    <script>
+    (function(h,e,a,t,m,p) {
+    m=e.createElement(a);m.async=!0;m.src=t;
+    p=e.getElementsByTagName(a)[0];p.parentNode.insertBefore(m,p);
+    })(window,document,'script','https://u.heatmap.it/log.js');
+    </script>
 </head>
 <body class="header-moved">
 
